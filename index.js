9e04471f7e698c53445107cb5877f803d1a5d157
azarateo@gmail.com <azarateo@gmail.com>
2023-02-23 15:52:33 +0800

    Adding axios and checking the first request to swapi/people

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const axios = require('axios')
 app = express()
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
 app.get('/', function (req, res) {
     res.render('home')
+})
+
+app.get('/characters/', function () {
+    const getCharacters = async () => {
+        try {
+            const res = await axios.get("https://swapi.dev/people")
+            const characters = res.data
+            console.log(characters)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    getCharacters()
+    res.render('characters')
 })
 
 app.listen(3000, () => {
