c9f1804ce82f97004285f2b734506b7f1a4325aa
azarateo@gmail.com <azarateo@gmail.com>
2023-02-24 08:36:37 +0800

    Finally working to print out the characters but no luck with the console

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// Creating variables that imports express and axios to make them available in this app
 const express = require('express')
 const axios = require('axios')
+// Creating the object express and assigning it to the variable app so I can use express to instantiate the server
 app = express()
+// 
 app.set('view engine', 'ejs')
 app.set('views', './views')
 const apiUrl = 'https://swapi.dev/api/people/';
@@ -10,13 +13,13 @@ app.get('/', function (req, res) {
 
 app.get('/characters', async (req, res) => {
     try {
-      const response = await axios.get(apiUrl);
-      res.render('characters', { response: response.data });
+        const response = await axios.get(apiUrl);
+        res.render('characters', { response: response.data });
     } catch (error) {
-      console.error(error);
-      res.status(500).send('An error occurred while fetching the response');
+        console.error(error);
+        res.status(500).send('An error occurred while fetching the response');
     }
-  });
+});
 
 app.listen(3000, () => {
     console.log("Server started on port 3000.")
