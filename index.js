a4083892bc33617d924e094c8e3b327318506946
azarateo <azarateo@gmail.com>
2023-03-27 08:57:54 +0800

    Server running again

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const axios = require('axios')
 const mongoose = require('mongoose')
 const path = require('path')
 // const bodyParser = require('body-parser')
-const mdburi = "mongodb+srv:/   /webdev:Bnds2023@cluster0.ymqk5yl.mongodb.net/?retryWrites=true&w=majority"
-// mongodb+srv://webdev:<password>@cluster0.ymqk5yl.mongodb.net/?retryWrites=true&w=majority
+const mdburi = "mongodb+srv://webdev:Bnds2023@cluster0.ymqk5yl.mongodb.net/?retryWrites=true&w=majority"
+//const mdburi = "mongodb+srv://webdev:<password>@cluster0.ymqk5yl.mongodb.net/?retryWrites=true&w=majority"
 app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, '/public')))
